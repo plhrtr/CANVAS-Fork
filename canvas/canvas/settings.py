e289faa440d3ae7248a9fdd3fcbd7e4aa0da0231
Markus Goetz <markus.goetz@kit.edu>
2024-03-06 11:25:37 +0100

    Added first url routes, added minimal template rendering

--- canvas/canvas/settings.py
+++ canvas/canvas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'editor'
 ]
 
 MIDDLEWARE = [
@@ -104,11 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
--- canvas/editor/views.py
+++ canvas/editor/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def editor(request):
+    return render(request, 'editor/editor.html')
