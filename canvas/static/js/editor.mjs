aa3e5c6388aece970e3889e7f1a3824cc59a48d5
Markus Goetz <markus.goetz@kit.edu>
2024-03-10 15:17:11 +0100

    First static scene rendering

--- canvas/static/js/editor.mjs
+++ canvas/static/js/editor.mjs
@@ -11,5 +11,25 @@ export class Editor {
         editorInstance = this;
 
         this.menu = new Menu();
+        this.canvas = document.getElementById("canvas");
+
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, this.canvas.clientWidth / this.canvas.clientHeight, 0.1, 1000);
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+        this.canvas.appendChild(this.renderer.domElement);
+
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        this.cube = new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.cube);
+
+        this.camera.position.y = -2;
+        this.camera.position.x = 3;
+        this.camera.position.z = 10;
+
+        requestAnimationFrame(() => {
+            this.renderer.render(this.scene, this.camera);
+        });
     }
 }
