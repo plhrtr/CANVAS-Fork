9a1c97872c56208d6e894e5a6087b989521c3842
Katharina Fluegel <katharina.fluegel@kit.edu>
2024-04-26 10:15:13 +0200

    Format code with standardJS and reduce new lines

--- canvas/static/js/compass.mjs
+++ canvas/static/js/compass.mjs
@@ -3,29 +3,29 @@
  */
 
 import {
-	BoxGeometry,
-	CanvasTexture,
-	Color,
-	Euler,
-	Mesh,
-	MeshBasicMaterial,
-	Object3D,
-	OrthographicCamera,
-	Quaternion,
-	Raycaster,
-	Sprite,
-	SpriteMaterial,
-	Vector2,
-	Vector3,
-	Vector4
+    BoxGeometry,
+    CanvasTexture,
+    Color,
+    Euler,
+    Mesh,
+    MeshBasicMaterial,
+    Object3D,
+    OrthographicCamera,
+    Quaternion,
+    Raycaster,
+    Sprite,
+    SpriteMaterial,
+    Vector2,
+    Vector3,
+    Vector4
 } from 'three';
 
 
 class CompassAxis {
     constructor(compass, axisId, color, label) {
         this.axisId = axisId;
-        this.axis = new Mesh( compass.geometry, this.getAxisMaterial(color) );
-        
+        this.axis = new Mesh(compass.geometry, this.getAxisMaterial(color));
+
         if (this.axisId == 'y') {
             this.axis.rotation.z = Math.PI / 2;
         } else if (this.axisId == 'z') {
@@ -34,21 +34,21 @@ class CompassAxis {
 
         compass.add(this.axis);
 
-        this.posAxisHelper = new Sprite( this.getSpriteMaterial( color, label ) );
-		this.posAxisHelper.userData.type = 'pos' + label;
-		
-        this.negAxisHelper = new Sprite( this.getSpriteMaterial( color ) );
-		this.negAxisHelper.userData.type = 'neg' + label;
+        this.posAxisHelper = new Sprite(this.getSpriteMaterial(color, label));
+        this.posAxisHelper.userData.type = 'pos' + label;
+
+        this.negAxisHelper = new Sprite(this.getSpriteMaterial(color));
+        this.negAxisHelper.userData.type = 'neg' + label;
 
         this.posAxisHelper.position[this.axisId] = 1;
         this.negAxisHelper.position[this.axisId] = - 1;
-        this.negAxisHelper.scale.setScalar( 0.8 );
+        this.negAxisHelper.scale.setScalar(0.8);
 
         compass.add(this.posAxisHelper);
         compass.add(this.negAxisHelper);
     }
 
-	dispose() {
+    dispose() {
         this.axis.material.dispose();
 
         this.posAxisHelper.material.map.dispose();
@@ -57,108 +57,100 @@ class CompassAxis {
         this.posAxisHelper.material.dispose();
         this.negAxisHelper.material.dispose();
     }
-    
+
     getInteractiveObjects() {
         return [this.posAxisHelper, this.negAxisHelper];
     }
 
     setOpacity(point) {
-        if ( point[this.axisId] >= 0 ) {
-
+        if (point[this.axisId] >= 0) {
             this.posAxisHelper.material.opacity = 1;
             this.negAxisHelper.material.opacity = 0.5;
-
         } else {
-
             this.posAxisHelper.material.opacity = 0.5;
             this.negAxisHelper.material.opacity = 1;
-
         }
     }
 
-    getAxisMaterial( color ) {
-        return new MeshBasicMaterial( { color: color, toneMapped: false } );
+    getAxisMaterial(color) {
+        return new MeshBasicMaterial({ color: color, toneMapped: false });
     }
 
-    getSpriteMaterial( color, text = null ) {
-        const canvas = document.createElement( 'canvas' );
+    getSpriteMaterial(color, text = null) {
+        const canvas = document.createElement('canvas');
         canvas.width = 64;
         canvas.height = 64;
 
-        const context = canvas.getContext( '2d' );
+        const context = canvas.getContext('2d');
         context.beginPath();
-        context.arc( 32, 32, 16, 0, 2 * Math.PI );
+        context.arc(32, 32, 16, 0, 2 * Math.PI);
         context.closePath();
         context.fillStyle = color.getStyle();
         context.fill();
 
-        if ( text !== null ) {
-
+        if (text !== null) {
             context.font = '24px Arial';
             context.textAlign = 'center';
             context.fillStyle = '#000000';
-            context.fillText( text, 32, 41 );
-
+            context.fillText(text, 32, 41);
         }
 
-        const texture = new CanvasTexture( canvas );
+        const texture = new CanvasTexture(canvas);
 
-        return new SpriteMaterial( { map: texture, toneMapped: false } );
+        return new SpriteMaterial({ map: texture, toneMapped: false });
     }
 }
 
 class ViewHelper extends Object3D {
 
-	constructor( camera, domElement ) {
+    constructor(camera, domElement) {
+        super();
+        this.camera = camera
+        this.domElement = domElement
 
-		super();
-		this.camera = camera
-		this.domElement = domElement
+        this.isViewHelper = true;
 
-		this.isViewHelper = true;
+        this.animating = false;
+        this.center = new Vector3();
 
-		this.animating = false;
-		this.center = new Vector3();
+        const color1 = new Color('#ff3653');
+        const color2 = new Color('#8adb00');
+        const color3 = new Color('#2c8fff');
 
-		const color1 = new Color( '#ff3653' );
-		const color2 = new Color( '#8adb00' );
-		const color3 = new Color( '#2c8fff' );
+        this.raycaster = new Raycaster();
+        this.mouse = new Vector2();
+        this.dummy = new Object3D();
 
-		this.raycaster = new Raycaster();
-		this.mouse = new Vector2();
-		this.dummy = new Object3D();
+        this.orthoCamera = new OrthographicCamera(- 2, 2, 2, - 2, 0, 4);
+        this.orthoCamera.position.set(0, 0, 2);
 
-		this.orthoCamera = new OrthographicCamera( - 2, 2, 2, - 2, 0, 4 );
-		this.orthoCamera.position.set( 0, 0, 2 );
-
-		this.geometry = new BoxGeometry( 0.8, 0.05, 0.05 ).translate( 0.4, 0, 0 );
+        this.geometry = new BoxGeometry(0.8, 0.05, 0.05).translate(0.4, 0, 0);
 
         this.xAxis = new CompassAxis(this, 'x', color1, 'X');
         this.yAxis = new CompassAxis(this, 'y', color2, 'Y');
         this.zAxis = new CompassAxis(this, 'z', color3, 'Z');
 
-		this.interactiveObjects = [].concat(this.xAxis.getInteractiveObjects(), this.yAxis.getInteractiveObjects(), this.yAxis.getInteractiveObjects());
+        this.interactiveObjects = [].concat(this.xAxis.getInteractiveObjects(), this.yAxis.getInteractiveObjects(), this.yAxis.getInteractiveObjects());
 
-		this.point = new Vector3();
-		this.dim = 128;
-		this.turnRate = 2 * Math.PI; // turn rate in angles per second
+        this.point = new Vector3();
+        this.dim = 128;
+        this.turnRate = 2 * Math.PI; // turn rate in angles per second
 
-		this.targetPosition = new Vector3();
-		this.targetQuaternion = new Quaternion();
+        this.targetPosition = new Vector3();
+        this.targetQuaternion = new Quaternion();
 
-		this.q1 = new Quaternion();
-		this.q2 = new Quaternion();
-		this.viewport = new Vector4();
-		this.radius = 0;
+        this.q1 = new Quaternion();
+        this.q2 = new Quaternion();
+        this.viewport = new Vector4();
+        this.radius = 0;
+    }
 
-	}
-
-	render( renderer ) {
-        this.quaternion.copy( this.camera.quaternion ).invert();
+    render(renderer) {
+        this.quaternion.copy(this.camera.quaternion).invert();
         this.updateMatrixWorld();
 
-        this.point.set( 0, 0, 1 );
-        this.point.applyQuaternion( this.camera.quaternion );
+        this.point.set(0, 0, 1);
+        this.point.applyQuaternion(this.camera.quaternion);
 
         this.xAxis.setOpacity(this.point);
         this.yAxis.setOpacity(this.point);
@@ -168,68 +160,56 @@ class ViewHelper extends Object3D {
 
         renderer.clearDepth();
 
-        renderer.getViewport( this.viewport );
-        renderer.setViewport( x, 0, this.dim, this.dim );
+        renderer.getViewport(this.viewport);
+        renderer.setViewport(x, 0, this.dim, this.dim);
 
-        renderer.render( this, this.orthoCamera );
+        renderer.render(this, this.orthoCamera);
 
-        renderer.setViewport( this.viewport.x, this.viewport.y, this.viewport.z, this.viewport.w );
-
+        renderer.setViewport(this.viewport.x, this.viewport.y, this.viewport.z, this.viewport.w);
     }
 
-	handleClick( event ) {
-
-        if ( this.animating === true ) return false;
+    handleClick(event) {
+        if (this.animating === true) return false;
 
         const rect = this.domElement.getBoundingClientRect();
-        const offsetX = rect.left + ( this.domElement.offsetWidth - this.dim );
-        const offsetY = rect.top + ( this.domElement.offsetHeight - this.dim );
-        this.mouse.x = ( ( event.clientX - offsetX ) / ( rect.right - offsetX ) ) * 2 - 1;
-        this.mouse.y = - ( ( event.clientY - offsetY ) / ( rect.bottom - offsetY ) ) * 2 + 1;
+        const offsetX = rect.left + (this.domElement.offsetWidth - this.dim);
+        const offsetY = rect.top + (this.domElement.offsetHeight - this.dim);
+        this.mouse.x = ((event.clientX - offsetX) / (rect.right - offsetX)) * 2 - 1;
+        this.mouse.y = - ((event.clientY - offsetY) / (rect.bottom - offsetY)) * 2 + 1;
 
-        this.raycaster.setFromCamera( this.mouse, this.orthoCamera );
+        this.raycaster.setFromCamera(this.mouse, this.orthoCamera);
 
-        const intersects = this.raycaster.intersectObjects( this.interactiveObjects );
+        const intersects = this.raycaster.intersectObjects(this.interactiveObjects);
 
-        if ( intersects.length > 0 ) {
-
-            const intersection = intersects[ 0 ];
+        if (intersects.length > 0) {
+            const intersection = intersects[0];
             const object = intersection.object;
 
-            this.prepareAnimationData( object, this.center );
+            this.prepareAnimationData(object, this.center);
 
             this.animating = true;
-
             return true;
-
         } else {
-
             return false;
-
         }
-
     }
 
-	update( delta ) {
+    update(delta) {
         const step = delta * this.turnRate;
 
         // animate position by doing a slerp and then scaling the position on the unit sphere
-
-        this.q1.rotateTowards( this.q2, this.step );
-        this.camera.position.set( 0, 0, 1 ).applyQuaternion( this.q1 ).multiplyScalar( this.radius ).add( this.center );
+        this.q1.rotateTowards(this.q2, this.step);
+        this.camera.position.set(0, 0, 1).applyQuaternion(this.q1).multiplyScalar(this.radius).add(this.center);
 
         // animate orientation
+        this.camera.quaternion.rotateTowards(this.targetQuaternion, step);
 
-        this.camera.quaternion.rotateTowards( this.targetQuaternion, step );
-
-        if ( this.q1.angleTo( this.q2 ) === 0 ) {
-
+        if (this.q1.angleTo(this.q2) === 0) {
             this.animating = false;
-
         }
     }
 
-	dispose() {
+    dispose() {
         this.geometry.dispose();
 
         this.xAxis.dispose();
@@ -237,58 +217,53 @@ class ViewHelper extends Object3D {
         this.zAxis.dispose();
     }
 
-    prepareAnimationData( object, focusPoint ) {
-        switch ( object.userData.type ) {
-
+    prepareAnimationData(object, focusPoint) {
+        switch (object.userData.type) {
             case 'posX':
-                this.targetPosition.set( 1, 0, 0 );
-                this.targetQuaternion.setFromEuler( new Euler( 0, Math.PI * 0.5, 0 ) );
+                this.targetPosition.set(1, 0, 0);
+                this.targetQuaternion.setFromEuler(new Euler(0, Math.PI * 0.5, 0));
                 break;
 
             case 'posY':
-                this.targetPosition.set( 0, 1, 0 );
-                this.targetQuaternion.setFromEuler( new Euler( - Math.PI * 0.5, 0, 0 ) );
+                this.targetPosition.set(0, 1, 0);
+                this.targetQuaternion.setFromEuler(new Euler(- Math.PI * 0.5, 0, 0));
                 break;
 
             case 'posZ':
-                this.targetPosition.set( 0, 0, 1 );
-                this.targetQuaternion.setFromEuler( new Euler() );
+                this.targetPosition.set(0, 0, 1);
+                this.targetQuaternion.setFromEuler(new Euler());
                 break;
 
             case 'negX':
-                this.targetPosition.set( - 1, 0, 0 );
-                this.targetQuaternion.setFromEuler( new Euler( 0, - Math.PI * 0.5, 0 ) );
+                this.targetPosition.set(- 1, 0, 0);
+                this.targetQuaternion.setFromEuler(new Euler(0, - Math.PI * 0.5, 0));
                 break;
 
             case 'negY':
-                targetPosition.set( 0, - 1, 0 );
-                targetQuaternion.setFromEuler( new Euler( Math.PI * 0.5, 0, 0 ) );
+                targetPosition.set(0, - 1, 0);
+                targetQuaternion.setFromEuler(new Euler(Math.PI * 0.5, 0, 0));
                 break;
 
             case 'negZ':
-                this.targetPosition.set( 0, 0, - 1 );
-                this.targetQuaternion.setFromEuler( new Euler( 0, Math.PI, 0 ) );
+                this.targetPosition.set(0, 0, - 1);
+                this.targetQuaternion.setFromEuler(new Euler(0, Math.PI, 0));
                 break;
 
             default:
-                console.error( 'ViewHelper: Invalid axis.' );
-
+                console.error('ViewHelper: Invalid axis.');
         }
 
-        //
+        radius = this.camera.position.distanceTo(focusPoint);
+        targetPosition.multiplyScalar(this.radius).add(focusPoint);
 
-        radius = this.camera.position.distanceTo( focusPoint );
-        targetPosition.multiplyScalar( this.radius ).add( focusPoint );
+        this.dummy.position.copy(focusPoint);
 
-        this.dummy.position.copy( focusPoint );
+        this.dummy.lookAt(this.camera.position);
+        q1.copy(this.dummy.quaternion);
 
-        this.dummy.lookAt( this.camera.position );
-        q1.copy( this.dummy.quaternion );
-
-        this.dummy.lookAt( this.targetPosition );
-        q2.copy( this.dummy.quaternion );
+        this.dummy.lookAt(this.targetPosition);
+        q2.copy(this.dummy.quaternion);
     }
-
 }
 
 export { ViewHelper };
--- canvas/static/js/editor.mjs
+++ canvas/static/js/editor.mjs
@@ -26,7 +26,7 @@ export class Editor {
         this.canvas.appendChild(this.renderer.domElement);
 
         this.geometry = new THREE.BoxGeometry(1, 1, 1);
-        this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         this.cube = new THREE.Mesh(this.geometry, this.material);
         this.scene.add(this.cube);
 
